ea80486eb6b2a67fe6e60cfec0b330d9fd2512c7
Leemyan <leemyan@naver.com>
2025-05-19 14:40:23 +0900

    Update practice.py

--- practice.py
+++ practice.py
@@ -211,12 +211,47 @@ Quiz) 당신은 최근에 코딩 스터디 모임을 새로 만들었습니다.
 
 '''슬라이싱'''
 
-jumin = "990120-1234567"
+# jumin = "990120-1234567"
 
-print("성별 : " + jumin[7]) # 1
-print("연도 : " + jumin[0:2]) # 0번째부터 2번째 직전까지
-print("월 : " + jumin[2:4]) # 01
-print("일자 : " + jumin[4:6]) # 20
+# print("성별 : " + jumin[7]) # 1
+# print("연도 : " + jumin[0:2]) # 0번째부터 2번째 직전까지
+# print("월 : " + jumin[2:4]) # 01
+# print("일자 : " + jumin[4:6]) # 20
 
-print("생년월일 : " + jumin[:6]) # 990120
-print("뒤 7자리 : " + jumin[7:]) # 1234567
+# print("생년월일 : " + jumin[:6]) # 990120
+# print("뒤 7자리 : " + jumin[7:]) # 1234567
+# print("뒤 7자리 (뒤에서부터) " + jumin[-7:]) # 맨 뒤에서 7번째부터 끝까지
+
+'''문자열 처리함수'''
+
+# python = "Python is Amazing"
+# print(python.lower())
+# print(python.upper())
+# print(python[0].isupper())
+# print(len(python))
+# print(python.replace("Python" , "Java"))
+
+# index = python.index("n")
+# print(index)
+# index = python.index("n", index + 1)
+# print(index)
+
+# print(python.find("n"))
+# print(python.find("Java")) # 없을 때 -1
+
+# print(python.find("Java"))
+# # print(python.index("Java"))
+# print("hi")
+
+# print(python.count("n"))
+
+
+'''문자열 포맷'''
+
+print("a" + "b")
+print("a","b")
+
+# 방법 1
+print("나는 %d살입니다." % 20)
+print("나는 %s을 좋아해요." % "파이썬")
+print("Apple 은 %c로 시작해요." % "A")
