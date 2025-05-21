5076fa69d4f2e3de399eb49b0bfd7cd8e1c0ba43
Leemyan <leemyan@naver.com>
2025-05-21 15:23:33 +0900

    0521 alpha

--- practice.py
+++ practice.py
@@ -248,10 +248,78 @@ Quiz) 당신은 최근에 코딩 스터디 모임을 새로 만들었습니다.
 
 '''문자열 포맷'''
 
-print("a" + "b")
-print("a","b")
+# print("a" + "b")
+# print("a","b")
 
-# 방법 1
-print("나는 %d살입니다." % 20)
-print("나는 %s을 좋아해요." % "파이썬")
-print("Apple 은 %c로 시작해요." % "A")
+# # 방법 1
+# print("나는 %d살입니다." % 20)
+# print("나는 %s을 좋아해요." % "파이썬")
+# print("Apple 은 %c로 시작해요." % "A")
+
+# # %s
+# print("나는 %s살입니다." % 20)
+# print("나는 %s색과 %s색을 좋아해요." % ("파란", "빨간"))
+
+# # 방법 2
+# print("나는 {}살 입니다." .format(20))
+# print("나는 {}색과 {}색을 좋아해요." .format("파란", "빨간"))
+# print("나는 {0}색과 {1}색을 좋아해요." .format("파란", "빨간"))
+# print("나는 {1}색과 {0}색을 좋아해요." .format("파란", "빨간"))
+
+# 방법 3
+# print("나는 {age}살이며, {color}색을 좋아해요" .format(age = 20, color = "빨간"))
+# print("나는 {age}살이며, {color}색을 좋아해요" .format(color = "빨간", age = 20))
+
+# # 방법 4
+# age = 20
+# color = "빨간"
+
+# print(f"나는 {age}살이며, {color}색을 좋아해요")
+
+# print("백문이 불여일견 \n백견이 불여일타")
+
+# # \" \" : 문장 내에서 따옴표
+# # 저는 "나도코딩"을 듣고있습니다
+# # print('저는 "나도코딩"을 듣고있습니다')
+# # print('저는 "나도코딩"을 듣고있습니다')
+# print("저는 \"나도코딩\"을 듣고있습니다")
+# print("저는 \'나도코딩\'을 듣고있습니다")
+
+# \\ : 문장 내에서 \ (하나로 바뀜)
+# print("C:\\User\\Nadocoding\\Desktop\\PythonWorkspace")
+
+# \r : 커서를 맨 앞으로 이동 
+# print("Red Apple\rPine")
+
+# \b : 백스페이스 (한 글자 삭제)
+# print("Redd\bApple")
+
+# \t : 탭
+# print("Red\tApple")
+
+'''
+Quiz) 사이트별로 비밀번호를 만들어주는 프로그램을 작성하시오
+
+예) http://naver.com
+규칙1 : http:// 부분은 제외 => naver.com
+규칙2 : 처음 만는 점(.) 이후 부분은 제외 => naver
+규칙3 : 남은 글자 중 처음 세자리 + 글자 갯수 + 글자 내 'e' 갯수 + "!" 로 구성
+                    (nav)     (5)           (1)        (!)
+
+예) 생성된 비밀번호 : nav51!
+'''
+# char = "http://naver.com"
+
+# print(char.)
+
+# url = "http://naver.com"
+# url = "http://google.com"
+# url = "http://daum.com"
+
+# my_str = url.replace("http://", "") # 규칙1
+# print(my_str)
+# my_str = my_str[:my_str.index(".")] # 규칙2
+# print(my_str)
+# password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!" #
+# print("{0}의 비밀번호는 {1} 입니다" .format(url ,password))
+
